a2d2fdd408cd16df7e2e37bff99b7db35582c5a9
babar <babar.shakeel515@gmail.com>
2021-04-08 19:17:50 +0500

    react-native-android-notification-listener added completed

---
+++ index.js
@@ -0,0 +1,56 @@
+/**
+ * @format
+ */
+import { AppRegistry } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { RNAndroidNotificationListenerHeadlessJsName } from 'react-native-android-notification-listener'
+
+
+import App from './App';
+import { name as appName } from './app.json';
+
+
+const headlessNotificationListener = async ({ notification }) => {
+    /**
+     * This notification is a JSON string in the follow format:
+     *  {
+     *      "app": string,
+     *      "title": string,
+     *      "titleBig": string,
+     *      "text": string,
+     *      "subText": string,
+     *      "summaryText": string,
+     *      "bigText": string,
+     *      "audioContentsURI": string,
+     *      "imageBackgroundURI": string,
+     *      "extraInfoText": string,
+     *      "groupedMessages": Array<Object> [
+     *          {
+     *              "title": string,
+     *              "text": string
+     *          }
+     *      ]
+     *  }
+     */
+
+    if (notification) {
+        /**
+         * Here you could store the notifications in a external API.
+         * I'm using AsyncStorage here as an example.
+         */
+        console.log(notification)
+        await AsyncStorage.setItem('@lastNotification', notification)
+    }
+}
+
+/**
+ * AppRegistry should be required early in the require sequence
+ * to make sure the JS execution environment is setup before other
+ * modules are required.
+ */
+AppRegistry.registerHeadlessTask(
+    RNAndroidNotificationListenerHeadlessJsName,
+    () => headlessNotificationListener
+)
+
+AppRegistry.registerComponent(appName, () => App)
